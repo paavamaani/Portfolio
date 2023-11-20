7f6ff21ea5f177b47a828ca60b48ad3289ae5bf7
Paavamaani Manchala <paavamaanimanchala@gmail.com>
2023-11-20 14:03:59 -0800

    changes for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      base: 'tailwindcss/base',
+      components: 'tailwindcss/components',
+      utilities: 'tailwindcss/utilities',
+    },
   },
   plugins: [],
   darkMode: 'class',
